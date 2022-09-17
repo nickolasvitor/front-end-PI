a451da9d34aedfb65b8a776c8f1c95e3804d33c3
NICKOLAS VITOR GOMES DA SILVA <nickolas.gomes@gcb.com.br>
2022-09-17 18:05:32 -0300

    inicio tela home

--- js/login.js
+++ js/login.js
@@ -6,33 +6,39 @@ function logar()
     var password = document.getElementById("password-input");
     
     console.log(email.value+password.value);
-
-    if(email.value=="admin@admin.com" && password.value=="admin")
+    if(email.value=="" || password.value=="")
     {
-        console.log("entrou")
-        console.log(email.value +" "+password.value)
-
-        localStorage.setItem("acesso",true);
-
-        location.href="homes.html";
-
-    
-        alert("Bem vindo")
-        
-     
-
-    }else
-    {
-        console.log("errou")
-
-        console.log(window.location.protocol)
-        console.log(window.location.host)
-        console.log(window.location.pathname)
-
-
-
-        alert("Usuário ou senha incorreto")
-     
+        alert("Por favor preencha os campos LOGIN e SENHA");
     }
 
+    else 
+    {
+        if(email.value=="admin@admin.com" && password.value=="admin")
+        {
+            console.log("entrou")
+            console.log(email.value +" "+password.value)
+
+            localStorage.setItem("acesso",true);
+
+            location.href="homes.html";
+
+        
+            alert("Bem vindo");
+            
+        
+
+        }else
+        {
+            console.log("errou")
+
+            console.log(window.location.protocol)
+            console.log(window.location.host)
+            console.log(window.location.pathname)
+
+
+
+            alert("Usuário ou senha incorreto")
+        
+        }
+    }
 };
