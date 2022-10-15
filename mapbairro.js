aaf022ee7b010e11e1c58bf4a87a39b1194009b5
NICKOLAS VITOR GOMES DA SILVA <nickolas.gomes@gcb.com.br>
2022-10-15 13:31:07 -0300

    Ajuste nos bairros dinamicos e adicionar botão de voltar

--- mapbairro.js
+++ mapbairro.js
@@ -4,9 +4,18 @@ var heatmap;
 
 function initMap(){
 
+    var url = window.location.search;
+    var urlParametro = new URLSearchParams(url);
+    var idBairro = urlParametro.get('tileId')
+    var idmatch = localStorage.getItem(idBairro)
+
+    idmatch=idmatch-1;
+
+    var latMAP = parseFloat(localStorage.getItem('lat'+idmatch))
+    var lngMAP = parseFloat(localStorage.getItem('lng'+idmatch))
     
     map= new google.maps.Map(document.getElementById("map"),{
-    center: {lat: -8.078734,lng: -34.909589},
+    center: {lat: latMAP,lng: lngMAP},
     zoom:15     });
     
     
