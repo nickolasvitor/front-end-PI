5f0cdb24d51642ba4924125e8d86e8b5c83c185e
NICKOLAS VITOR GOMES DA SILVA <nickolas.gomes@gcb.com.br>
2022-10-19 10:25:07 -0300

    ajustando pwabuilder

--- sw.js
+++ sw.js
@@ -56,7 +56,14 @@ self.addEventListener('fetch', function (event) {
      }
    }());
 
-   
+   async function requestBackgroundSync() {
+    await self.registration.sync.register('my-tag-name');
+}
+self.addEventListener('sync', event => {
+  if (event.tag === 'my-tag-name') {
+      event.waitUntil(doTheWork());
+  }
+});
 
   //Atualizacao cache
   /*event.respondWith(
