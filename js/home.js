9b4cece1e5d997ce316fe32b623de9b7327cd5bc
NICKOLAS VITOR GOMES DA SILVA <nickolas.gomes@gcb.com.br>
2022-10-19 15:02:40 -0300

    ajustes finais

--- js/home.js
+++ js/home.js
@@ -75,8 +75,32 @@ addEventListener("load",function(){
 
 })
 
+//script para fazer o popup do filtro
+$(document).ready(function(){
 
+  $('div.dropdown').each(function() {
+    var $dropdown = $(this);
 
+    $("a.dropdown-link", $dropdown).click(function(e) {
+      e.preventDefault();
+      $div = $("div.dropdown-container", $dropdown);
+      $div.toggle();
+      $("div.dropdown-container").not($div).hide();
+      return false;
+    });
+
+});
+    
+  $('html').click(function(){
+    $("div.dropdown-container").hide();
+  });
+     
+});
+
+function mensagemsucesso(){
+  window.alert("Bueiro cadastrado com sucesso");
+  location.href="homes.html";
+}
 
 
 
